004380b77ae4a4f209f92feda9d0f9b96cc8e50b
TristanCode <tristanhschmid@gmail.com>
2024-09-16 10:16:45 +0200

    update to comments

--- Classes/API_Requests.py
+++ Classes/API_Requests.py
@@ -23,10 +23,8 @@ class API_Requests:
         headers["Content-type"] = content_type
         if authorization_token is not None:
             headers["Authorization"] = authorization_token
-        #In testing, I found that my test, companie house, wouldn't except this, so 
+        #In testing, I found that my test, companie house, wouldn't except the api key in headers, so 
         #I have chosen to use auth=(api_key,"") instead
-        #if api_key is not None :
-            #headers["x-api-key"] = api_key
         result = requests.request(
             url= url,
             method= method,
@@ -36,14 +34,14 @@ class API_Requests:
         if not result:
             #I've used this method instead of json to restrict unsesesary imports
             if result.text.__contains__("error"):
-                #returns the error thrown bye the API
+                #prints the error sent by the API
                 start_index_of_error = result.text.find("error") + 8
                 end_index_of_error = result.text.find("\"", start_index_of_error, result.text.__len__())
                 print("Reached API with error: " +result.text[start_index_of_error: end_index_of_error])
                 return None
             
             elif result.text.__contains__("message"):
-                #returns the fair
+                #prints the message sent by the API
                 start_index_of_error = result.text.find("message") + 10
                 end_index_of_error = result.text.find("\"", start_index_of_error, result.text.__len__())
                 print("failed to reach API, message: " +result.text[start_index_of_error: end_index_of_error])
